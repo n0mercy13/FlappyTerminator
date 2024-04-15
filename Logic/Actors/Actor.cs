791188da5e8c4aaf366e11a36acb13857247092e
nomercy <hentai_ai@mail.ru>
2024-04-15 17:37:10 +0300

    Actor amended;

--- Logic/Actors/Actor.cs
+++ Logic/Actors/Actor.cs
@@ -1,21 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
+using Codebase.Infrastructure;
 
 namespace Codebase.Logic
 {
     public partial class Actor : MonoBehaviour
     {
+        private IEnergy _energy;
+
+        public event Action<int, int> EnergyChanged = delegate { };
+
+        private void Awake()
+        {
+            _energy = new Energy();
+        }
+
+        private void OnEnable()
+        {
+            _energy.Depleted += OnEnergyDepleted;
+            _energy.Changed += OnEnergyChanged;
+        }
+
+        private void OnDisable()
+        {
+            _energy.Depleted -= OnEnergyDepleted;
+            _energy.Changed -= OnEnergyChanged;
+        }
+
+        protected void SetMaxHealth(int maxHealth)
+        {
+            if (_energy is IInitializable<int> initializable)
+                initializable.Initialize(maxHealth);
+        }
+
+        protected virtual void OnEnergyDepleted()
+        {
+        }
+
+        private void OnEnergyChanged(int energy, int maxEnergy) => 
+            EnergyChanged.Invoke(energy, maxEnergy);
     }
 
     public partial class Actor : IDamageable
     {
         public void ApplyDamage(int amount)
         {
-            throw new NotImplementedException();
+            _energy.Increase(amount);
         }
     }
 }
